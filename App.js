12a98a84f7b12d610d5914f18a33b29deb0f58c9
unknown <maninamastereact@gmail.com>
2023-07-12 10:51:56 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},React.createElement("h1",{},"React heading element inside div")))
+
+
+const heading = React.createElement(
+    "h1",
+    {id:"head",abc:"xyz"},
+    "Hello world from React"
+)
+
+const bigparent = React.createElement("div",{class:"parent"},
+[React.createElement("div",{class:"child 1"},[React.createElement("h1",{},"Hello h1"),React.createElement("h2",{},"Hello h2")]),
+React.createElement("div",{class:"child 2"},[React.createElement("h1",{},"Hello h1"),React.createElement("h2",{},"Hello h2")])]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(bigparent)
+
+
